fecd0608a99250b0eb7617c75f860807101f7053
Saint <arishabh748@gmail.com>
2022-05-15 10:12:33 +0530

    Formatted
    Signed-off-by: Saint <arishabh748@gmail.com>

--- home.js
+++ home.js
@@ -1,35 +1,41 @@
 function copyBladeLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro-blade.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro-blade.min.css"
+    )
 }
+
 function copyBuffyLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro-buffy.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro-buffy.min.css"
+    )
 }
+
 function copyLincolnLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro-lincoln.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro-lincoln.min.css"
+    )
 }
+
 function copyMorbiusLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro-morbius.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro-morbius.min.css"
+    )
 }
+
 function copyVanHelsingLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro-van-helsing.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro-van-helsing.min.css"
+    )
 }
+
 function copyProLink() {
-	navigator.clipboard.writeText(
-		"https://saintwithataint.github.io/cbplus/dracula-pro.min.css"
-	);
+    navigator.clipboard.writeText(
+        "https://saintwithataint.github.io/cbplus/dracula-pro.min.css"
+    )
 }
+
 function copyCustomCssImport() {
-	navigator.clipboard.writeText(
-		"@import url('https://saintwithataint.github.io/customCSS/custom.css');"
-	);
+    navigator.clipboard.writeText(
+        "@import url('https://saintwithataint.github.io/customCSS/custom.css');"
+    )
 }
